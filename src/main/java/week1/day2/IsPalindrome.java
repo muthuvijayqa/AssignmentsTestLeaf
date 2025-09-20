89436d218c1134751cdf98bae2f935918e1da68c
muges <muges@LAPTOP-O4K0FNH1>
2025-09-20 12:12:48 +0530

    Format changed

--- src/main/java/week1/day2/IsPalindrome.java
+++ src/main/java/week1/day2/IsPalindrome.java
@@ -5,34 +5,33 @@ import java.util.Scanner;
 public class IsPalindrome {
 
 	public static void main(String[] args) {
-		
-		
-		//Scanner Object o read the input from the user
+
+		// Scanner Object o read the input from the user
 		Scanner sc = new Scanner(System.in);
-		
-		//Ask the user to enter the number
+
+		// Ask the user to enter the number
 		System.out.println("Enter the number:");
-		
-		//Store the input from user to variable num
+
+		// Store the input from user to variable num
 		int num = sc.nextInt();
-		
-		//declare and initialize a variable to store the reversed number
-		
+
+		// declare and initialize a variable to store the reversed number
+
 		int rev = 0;
-		
-		//loop to get the reversed number 
-		
-		for (int i = num; i > 0; i=i/10) {
-			
-			//shift the digit in rev to one place in left
-			//Get the last digit of the number(i%10)
-			rev = rev*10 + (i%10);
+
+		// loop to get the reversed number
+
+		for (int i = num; i > 0; i = i / 10) {
+
+			// shift the digit in rev to one place in left
+			// Get the last digit of the number(i%10)
+			rev = rev * 10 + (i % 10);
 		}
-		
-		//check if original number and reversed number are same
-		if (num==rev) {
+
+		// check if original number and reversed number are same
+		if (num == rev) {
 			System.out.println("The given number is a palindrome");
-		}else {
+		} else {
 			System.out.println("The given number is not a palindrome");
 		}
 	}
--- src/main/java/week1/day2/IsPrime.java
+++ src/main/java/week1/day2/IsPrime.java
@@ -5,56 +5,52 @@ import java.util.Scanner;
 public class IsPrime {
 
 	public static void main(String[] args) {
-		
-		
-		//Scanner Object to get input from the user
-		
+
+		// Scanner Object to get input from the user
+
 		Scanner sc = new Scanner(System.in);
-		
-		//Ask the user to key the number
+
+		// Ask the user to key the number
 		System.out.println("Enter the number: ");
-		
+
 		// store the input value to variable "num"
 		int num = sc.nextInt();
-		
-		//Declaring and initializing a variable "Count" to store the number of factors/Divisors
-		
+
+		// Declaring and initializing a variable "Count" to store the number of
+		// factors/Divisors
+
 		int count = 0;
-		
-		
-		//check if num is greater than 1
-		if(num>1) {
-			
-		//loop to find the factors of the given number
+
+		// check if num is greater than 1
+		if (num > 1) {
+
+			// loop to find the factors of the given number
 			for (int i = 1; i <= num; i++) {
-				
-				//increment the value of count if the given number is completly divisble by i
-				if (num%i==0) {
-					
+
+				// increment the value of count if the given number is completly divisble by i
+				if (num % i == 0) {
+
 					count++;
-					
+
 				}
-				
-				
-				
+
 			}
-			
-			
+
 		}
-		
-		//If the number of factors is only 2, the given number is printed as Prime
-		if (count==2) {
-			
+
+		// If the number of factors is only 2, the given number is printed as Prime
+		if (count == 2) {
+
 			System.out.println("The given number is a prime number");
-			
-		} 
-		
-		// if the number of factors is not equal to 2, the given number is printed as not a prime number
+
+		}
+
+		// if the number of factors is not equal to 2, the given number is printed as
+		// not a prime number
 		else {
-			
+
 			System.out.println("The given number is not a prime number");
 		}
-		
 
 	}
 

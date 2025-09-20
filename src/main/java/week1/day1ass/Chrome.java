6dbb546328c69d7aef3bfe282aeded6829798334
muges <muges@LAPTOP-O4K0FNH1>
2025-09-20 07:00:27 +0530

    week1assignments

---
+++ src/main/java/week1/day1ass/Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1ass;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+	
+				//declaring and initializing variable
+				float version = 91.0f;
+				String developer = "Google";
+				boolean isBeta = false;
+				int releaseYear = 2008;
+				char shortcutKey = 'c';
+				
+				//Print all the variables in seperate lines
+				System.out.println("Version is "+version);
+				System.out.println("developer is "+developer);
+				System.out.println("isBeta "+isBeta );
+				System.out.println("releaseYear is"+ releaseYear);
+				System.out.println("shortcutKey is"+shortcutKey);
+
+	}
+
+}
